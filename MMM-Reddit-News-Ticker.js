f2d5673f7fd9261528fe60ca5b45fc00956f23bc
Clinton Thomas <29966176+hayseedbyte@users.noreply.github.com>
2022-08-17 15:13:12 -0400

    reddit api
    user client id and secret now added to config block.

--- MMM-Reddit-News-Ticker.js
+++ MMM-Reddit-News-Ticker.js
@@ -1,39 +1,42 @@
 /* eslint-disable indent */
 /* eslint-disable prettier/prettier */
-Module.register("MMM-Reddit-News-Ticker", {
+Module.register('MMM-Reddit-News-Ticker', {
   defaults: {
     updateInterval: 9000000, // 2.5 hours
-    retryDelay: 5000
+    retryDelay: 5000,
   },
 
-  requiresVersion: "2.1.0", // Required version of MagicMirror
+  requiresVersion: '2.1.0', // Required version of MagicMirror
 
   start: function () {
     var home = this;
-    console.log(home.name + "START");
-    home.getData();
+    console.log(home.name + 'START');
+    const id = home.config.client_id;
+    const secret = home.config.secret;
+    const api = [id, secret];
+    home.getData(api);
     setInterval(function () {
       self.updateDom();
     }, home.config.updateInterval);
   },
 
-  getData: function () {
+  getData: function (data) {
     const retry = true;
-    const data = this.data;
-    this.sendSocketNotification("PULL_NEWS", null);
+    // const data = this.data;
+    this.sendSocketNotification('PULL_NEWS', data);
   },
   getDom: function () {
-    const moduleDiv = document.createElement("div");
-    if (document.querySelector(".hwrap") === null) {
-      const hwrap = document.createElement("div");
-      const hmove = document.createElement("div");
-      const hitem = document.createElement("div");
+    const moduleDiv = document.createElement('div');
+    if (document.querySelector('.hwrap') === null) {
+      const hwrap = document.createElement('div');
+      const hmove = document.createElement('div');
+      const hitem = document.createElement('div');
       moduleDiv.appendChild(hwrap);
       hwrap.appendChild(hmove);
       hmove.appendChild(hitem);
-      hwrap.classList.add("hwrap");
-      hmove.classList.add("hmove");
-      hitem.classList.add("hitem");
+      hwrap.classList.add('hwrap');
+      hmove.classList.add('hmove');
+      hitem.classList.add('hitem');
     }
     return moduleDiv;
   },
@@ -43,46 +46,46 @@ Module.register("MMM-Reddit-News-Ticker", {
   },
 
   getStyles: function () {
-    return ["MMM-Reddit-News-Ticker.css"];
+    return ['MMM-Reddit-News-Ticker.css'];
   },
 
   // Load translations files
   getTranslations: function () {
     return {
-      en: "translations/en.json",
-      es: "translations/es.json"
+      en: 'translations/en.json',
+      es: 'translations/es.json',
     };
   },
 
   processData: function (data) {
     this.loaded = true;
-    this.sendSocketNotification("DOM_OBJECTS_CREATED", data);
+    this.sendSocketNotification('DOM_OBJECTS_CREATED', data);
   },
   injectHTML: function (str) {
-    const hitem = document.querySelector(".hitem");
+    const hitem = document.querySelector('.hitem');
     hitem.innerHTML = str;
   },
 
   socketNotificationReceived: function (notification, payload) {
     const self = this;
     switch (notification) {
-      case "TIME":
-        console.log(this.name + " ⌛");
+      case 'TIME':
+        console.log(this.name + ' ⌛');
         const time = payload;
-        const hitem = document.querySelector(".hitem");
-        hitem.style.animationName = "tickerh";
+        const hitem = document.querySelector('.hitem');
+        hitem.style.animationName = 'tickerh';
         hitem.style.animationDuration = `${time}s`;
-        hitem.style.animationTimingFunction = "linear";
-        hitem.style.animationIterationCount = "infinite";
+        hitem.style.animationTimingFunction = 'linear';
+        hitem.style.animationIterationCount = 'infinite';
         break;
-      case "ARR":
-        console.log(this.name + " 🏴‍☠️");
+      case 'ARR':
+        console.log(this.name + ' 🏴‍☠️');
         this.injectHTML(payload);
         break;
-      case "TITLES":
-        console.log(this.name + " 📰");
+      case 'TITLES':
+        console.log(this.name + ' 📰');
         this.processData(payload);
         break;
     }
-  }
+  },
 });
--- node_helper.js
+++ node_helper.js
@@ -1,35 +1,37 @@
-var NodeHelper = require("node_helper");
-const cp = require("child_process");
+var NodeHelper = require('node_helper');
+const cp = require('child_process');
 module.exports = NodeHelper.create({
   socketNotificationReceived: function (notification, payload) {
-    if (notification === "PULL_NEWS") {
+    if (notification === 'PULL_NEWS') {
       const execOptions = {
-        cwd: "/home/pi/MagicMirror/modules/MMM-Reddit-News-Ticker/",
+        cwd: '/home/pi/MagicMirror/modules/MMM-Reddit-News-Ticker/',
         env: null,
-        encoding: "utf8",
-        timeout: 0
+        encoding: 'utf8',
+        timeout: 0,
       };
+      const id = payload[0];
+      const secret = payload[1];
       const out = cp.exec(
-        "/usr/bin/python3 /home/pi/MagicMirror/modules/MMM-Reddit-News-Ticker/reddit-news-str.py",
+        `/usr/bin/python3 /home/pi/MagicMirror/modules/MMM-Reddit-News-Ticker/reddit-news-str.py ${id} ${secret}`,
         execOptions,
         (err, stdout, stderr) => {
-          this.sendSocketNotification("TITLES", stdout);
+          this.sendSocketNotification('TITLES', stdout);
         }
       );
     }
-    if (notification === "DOM_OBJECTS_CREATED") {
+    if (notification === 'DOM_OBJECTS_CREATED') {
       const data = payload;
       const string = data.toString();
       const length = string.length;
       const time = Math.round(length / 12);
-      this.sendSocketNotification("TIME", time);
+      this.sendSocketNotification('TIME', time);
       this.joinData(data);
     }
   },
 
   joinData: function (arr) {
-    const ray = arr.split("~");
-    const joined = ray.join("&nbsp&nbsp&nbsp&nbsp::&nbsp&nbsp&nbsp&nbsp");
-    this.sendSocketNotification("ARR", joined);
-  }
+    const ray = arr.split('~');
+    const joined = ray.join('&nbsp&nbsp&nbsp&nbsp::&nbsp&nbsp&nbsp&nbsp');
+    this.sendSocketNotification('ARR', joined);
+  },
 });
